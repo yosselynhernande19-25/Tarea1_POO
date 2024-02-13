bad9ef9a54938afb96b482165b3f2fbb386442c5
Yosselyn Hernández <140340196+yosselynhernande19-25@users.noreply.github.com>
2024-02-13 12:23:27 -0600

    Add files via upload

---
+++ Lab1/Libro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+   
+using System.Collections.Generic;
+
+public abstract class Libro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AñoPublicacion { get; set; }
+        public double Precio { get; set; }
+
+        public Libro(string titulo, string autor, int añoPublicacion, double precio)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            AñoPublicacion = añoPublicacion;
+            Precio = precio;
+        }
+
+        public abstract void Describir();
+
+        public static double CalcularPrecioTotal(List<Libro> libros)
+        {
+            double total = 0;
+            foreach (var libro in libros)
+            {
+                total += libro.Precio;
+            }
+            return total;
+        }
+    }
+
+
+}
+
---
+++ Lab1/LibroDigital.cs
@@ -0,0 +1,28 @@
+﻿using Lab1;
+using System;
+
+public class LibroDigital : Libro
+{
+    public string Formato { get; set; }
+    public int NumeroDePaginas { get; set; }
+
+    public LibroDigital(string titulo, string autor, int añoPublicacion, double precio, string formato, int numeroDePaginas)
+        : base(titulo, autor, añoPublicacion, precio)
+    {
+        Formato = formato;
+        NumeroDePaginas = numeroDePaginas;
+    }
+
+    public override void Describir()
+    {
+        Console.WriteLine($"Título: {Titulo}");
+        Console.WriteLine($"Autor: {Autor}");
+        Console.WriteLine($"Año de publicación: {AñoPublicacion}");
+        Console.WriteLine($"Precio: {Precio:C}");
+        Console.WriteLine($"Formato: {Formato}");
+        Console.WriteLine($"Número de páginas: {NumeroDePaginas}");
+    }
+}
+
+
+
