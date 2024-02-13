cff1f746944110d116a5162563133a007f010578
Yosselyn Hernández <140340196+yosselynhernande19-25@users.noreply.github.com>
2024-02-13 12:36:15 -0600

    Add files via upload

---
+++ Lab1/LibroImpreso.cs
@@ -0,0 +1,25 @@
+﻿using Lab1;
+using System;
+
+public class LibroImpreso : Libro
+{
+    public int NumeroDePaginas { get; set; }
+
+    public LibroImpreso(string titulo, string autor, int añoPublicacion, double precio, int numeroDePaginas)
+        : base(titulo, autor, añoPublicacion, precio)
+    {
+        NumeroDePaginas = numeroDePaginas;
+    }
+
+    public override void Describir()
+    {
+        Console.WriteLine($"Título: {Titulo}");
+        Console.WriteLine($"Autor: {Autor}");
+        Console.WriteLine($"Año de publicación: {AñoPublicacion}");
+        Console.WriteLine($"Precio: {Precio:C}");
+        Console.WriteLine($"Número de páginas: {NumeroDePaginas}");
+    }
+}
+
+
+
